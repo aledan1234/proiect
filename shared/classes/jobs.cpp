ebaafb743aca203dd55cd393c90b1d5f7b952b41
aledan1234 <109103951+aledan1234@users.noreply.github.com>
2024-06-23 20:26:36 +0300

    Update jobs.cpp
    //am schimbat jobs la job, nu stiu de ce, dar altfel da eroare la citire, ceva de suprapunere, nu s sigur

--- shared/classes/jobs.cpp
+++ shared/classes/jobs.cpp
@@ -1,5 +1,5 @@
 #include "jobs.hpp"
-
+/*
 Jobs::Jobs(int id, string compania, string pozitia, int experienta, vector<string> skills, int nrCandidati, Candidat candidati[])
 : id(id), compania(compania), pozitia(pozitia), skills(skills), experienta(experienta), nrCandidati(nrCandidati) {
 
@@ -11,4 +11,48 @@ Jobs::Jobs(int id, string compania, string pozitia, int experienta, vector<strin
 
 Jobs::~Jobs() {
     //delete[] candidati;
+}
+*/
+//am schimbat jobs la job, nu stiu de ce, dar altfel da eroare la citire, ceva de suprapunere, nu s sigur
+#include "jobs.hpp"
+
+Job::Job(int id, string compania, string pozitia, int experienta, vector<string> skills, int nrCandidati, Candidat candidati[])
+: id(id), compania(compania), pozitia(pozitia), skills(skills), experienta(experienta), nrCandidati(nrCandidati) {
+
+    for(int i = 0; i < nrCandidati; ++i) {
+        this->candidati[i] = candidati[i];
+    }
+
+}
+
+Job::~Job() {
+    //delete[] candidati;
+}
+
+int Job::getId() const {
+    return id;
+}
+
+string Job::getCompania() const {
+    return compania;
+}
+
+string Job::getPozitia() const {
+    return pozitia;
+}
+
+int Job::getExperienta() const {
+    return experienta;
+}
+
+vector<string> Job::getSkills() const {
+    return skills;
+}
+
+int Job::getNrCandidati() const {
+    return nrCandidati;
+}
+
+vector<Candidat> Job::getCandidati() const {
+    return vector<Candidat>(candidati, candidati + nrCandidati);
 }
