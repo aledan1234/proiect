20c373fc4edb0228a54d2f4b02cde0482c5490aa
aledan1234 <109103951+aledan1234@users.noreply.github.com>
2024-06-23 20:24:20 +0300

    Update app2.cpp
    Am schimbat totul sa fie cu argumente, sper ca e ok, citeste momenten fisierele cu jobs, mai am pobleme la cititul candidatilor, dar rezolv si asta

--- app2.cpp
+++ app2.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <fstream>
 #include <algorithm>
+//#include "C:\Users\alexc\OneDrive\Desktop\shared\classes\jobs.hpp" //g++ -o app app2.cpp
 #include "./shared/classes/jobs.hpp"
 
 using namespace std;
@@ -10,6 +11,7 @@ using namespace std;
 vector<Job> loadJobs(const string& filename) {
     vector<Job> jobs;
     ifstream file(filename);
+    /*
     if (file.is_open()) {
         int id;
         string title;
@@ -26,24 +28,42 @@ vector<Job> loadJobs(const string& filename) {
             jobs.push_back(job);
         }
         file.close();
+    }*/
+    if (file.is_open()) {
+        int id;
+        string compania, pozitia, title;
+        int experienta, nrCandidati, numSkills;
+        while (file >> id >> compania >> pozitia >> experienta >> numSkills) {
+            vector<string> skills;
+            for (int i = 0; i < numSkills; ++i) {
+                string skill;
+                file >> skill;
+                skills.push_back(skill);
+            }
+            Candidat candidati[900];
+            jobs.emplace_back(id, compania, pozitia, experienta, skills, nrCandidati, candidati);
+        }
+        file.close();
     }
-    return jobs;
+
+    return jobs;  //g++ -o app view_jobs
 }
 
 void displayJobs(const vector<Job>& jobs) {
     for (const auto& job : jobs) {
-        cout << "Job ID: " << job.id << ", Title: " << job.title << ", Skills: ";
-        for (const auto& skill : job.skills) {
+        cout << "Job ID: " << job.getId() << ", Company: " << job.getCompania() << ", Position: " << job.getPozitia() << endl;
+        cout << "Skills: ";
+        for (const auto& skill : job.getSkills()) {
             cout << skill << " ";
         }
         cout << endl;
     }
 }
 
-vector<Job> filterJobsBySkill(const vector<Job>& jobs, const string& skill) {
+vector<Job> filterJobsBySkill(const vector<Job>& jobs, const string& skill) { //app.exe filter_jobs C++ sau g++ -o app filter_jobs
     vector<Job> filteredJobs;
     for (const auto& job : jobs) {
-        if (find(job.skills.begin(), job.skills.end(), skill) != job.skills.end()) {
+        if (find(job.getSkills().begin(), job.getSkills().end(), skill) != job.getSkills().end()) {
             filteredJobs.push_back(job);
         }
     }
@@ -61,43 +81,34 @@ void applyForJob(const string& filename, int jobId, const string& applicantName)
     }
 }
 
-int main(int args, char *argv[])
-{
-    cout << "args= " << args << " and argv[] " << argv[1] << endl
-         << endl;
+int main(int argc, char* argv[]) {
     vector<Job> jobs = loadJobs("jobs.txt");
 
-    int choice;
-    while (true) {
-        cout << "1. View all jobs" << endl;
-        cout << "2. Filter jobs by skill" << endl;
-        cout << "3. Apply for a job" << endl;
-        cout << "4. Exit" << endl;
-        cout << "Enter your choice: ";
-        cin >> choice;
+    if (argc < 2) {
+        cout << "Usage: " << argv[0] << " <command> [options]" << endl;
+        cout << "Commands:" << endl;
+        cout << "  view_jobs" << endl;
+        cout << "  filter_jobs <skill>" << endl;
+        cout << "  apply_job <job_id> <applicant_name>" << endl;
+        return 1;
+    }
 
-        if (choice == 1) {
-            displayJobs(jobs);
-        } else if (choice == 2) {
-            string skill;
-            cout << "Enter skill to filter by: ";
-            cin >> skill;
-            vector<Job> filteredJobs = filterJobsBySkill(jobs, skill);
-            displayJobs(filteredJobs);
-        } else if (choice == 3) {
-            int jobId;
-            string applicantName;
-            cout << "Enter Job ID to apply for: ";
-            cin >> jobId;
-            cout << "Enter your name: ";
-            cin >> applicantName;
-            applyForJob("applications.txt", jobId, applicantName);
-        } else if (choice == 4) {
-            break;
-        } else {
-            cout << "Invalid choice. Please try again." << endl;
-        }
+    string command = argv[1];
+
+    if (command == "view_jobs") {
+        displayJobs(jobs);
+    } else if (command == "filter_jobs" && argc == 3) {
+        string skill = argv[2];
+        vector<Job> filteredJobs = filterJobsBySkill(jobs, skill);
+        displayJobs(filteredJobs);
+    } else if (command == "apply_job" && argc == 4) {
+        int jobId = stoi(argv[2]);
+        string applicantName = argv[3];
+        applyForJob("applications.txt", jobId, applicantName);
+    } else {
+        cout << "Invalid command or arguments." << endl;
     }
 
     return 0;
 }
+
