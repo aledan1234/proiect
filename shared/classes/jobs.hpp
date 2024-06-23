48f62d5bd69d4ac6bfc8bb399718161149c05903
aledan1234 <109103951+aledan1234@users.noreply.github.com>
2024-06-23 20:29:03 +0300

    Update jobs.hpp
    am pus astea cu int get... const pentru ca alea initial sunt private, asa vom putea accesa datele

--- shared/classes/jobs.hpp
+++ shared/classes/jobs.hpp
@@ -1,13 +1,14 @@
 #ifndef JOBS_MANAGEMENT_HPP
 #define JOBS_MANAGEMENT_HPP
 
+#include <iostream>
 #include <vector>
 #include <string>
 #include "candidat.hpp"
 
 using namespace std;
 
-class Jobs {
+class Job {
 private:
     int id;
     string compania;
@@ -18,10 +19,21 @@ private:
     Candidat candidati[900];
 
 public:
-    Jobs(int id, string compania, string pozitia, int experienta, vector<string> skills, int nrCandidati, Candidat candidati[]);
+    Job(int id, string compania, string pozitia, int experienta, vector<string> skills, int nrCandidati, Candidat candidati[]);
 
-    ~Jobs();
+    ~Job();
+
+    int getId() const;
+    string getCompania() const;
+    string getPozitia() const;
+    int getExperienta() const;
+    vector<string> getSkills() const;
+    int getNrCandidati() const;
+    vector<Candidat> getCandidati() const;
 };
+
+#endif
+
 
 // old code bellow
 
